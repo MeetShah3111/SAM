5290351ed3086b82cc31d22f228047dffd7fd86f
Shah Meet <shahmeet@gmail.com>
2024-11-14 12:47:07 +0530

    meetshah

---
+++ Sampul/q1.py
@@ -0,0 +1,9 @@
+print("enter the vector1")
+print("enter the vector2")
+x1=int(input("enter x1"))
+x2=int(input("enter x2")) 
+x3=int(input("enter x3"))
+y1=int(input("enter y1"))
+y2=int(input("enter y2"))
+print("Hello")
+
